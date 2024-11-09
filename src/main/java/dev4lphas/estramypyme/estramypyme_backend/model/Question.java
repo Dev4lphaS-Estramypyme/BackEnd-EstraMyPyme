402b6bf3bd3e300bcb765db3ed5d23d66637e061
Santiago <ardyosa@hotmail.com>
2024-11-08 19:59:46 -0500

    "feat(crud): creación del crud para question,"

--- src/main/java/dev4lphas/estramypyme/estramypyme_backend/model/Question.java
+++ src/main/java/dev4lphas/estramypyme/estramypyme_backend/model/Question.java
@@ -1,5 +1,33 @@
 package dev4lphas.estramypyme.estramypyme_backend.model;
 
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "questions")
 public class Question {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "question", nullable = false)
+    private String question;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "created_at", nullable = false)
+    private LocalDate createdAt;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active = true;
+
 }
--- src/main/java/dev4lphas/estramypyme/estramypyme_backend/repository/QuestionRepository.java
+++ src/main/java/dev4lphas/estramypyme/estramypyme_backend/repository/QuestionRepository.java
@@ -1,5 +1,10 @@
 package dev4lphas.estramypyme.estramypyme_backend.repository;
 
-public interface QuestionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import dev4lphas.estramypyme.estramypyme_backend.model.Question;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
     
 }
